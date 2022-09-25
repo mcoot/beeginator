12bd60d4ec46b331e8c4aae4a3788efe7aa31878
Joseph Spearritt <joe.spearritt@live.com>
2022-09-25 21:43:05 +1000

    Save emoji in a map, add todo list

---
+++ TODO.md
@@ -0,0 +1,23 @@
+# Todo list
+
+All this is a random hack out of boredom while in covid iso so dunno that I'll get to much of this.
+
+## Basic functionality
+
+* Larger (resizeable? full-page?) work grid area
+* Actual layout
+* Renderer automatically takes the smallest area containing emoji
+* Take emoji list from a file (probably ok for now - integrating with slack API directly probably too complicated auth-wise for this revision)
+* Allow configuring what the blank emoji is
+* Re-work tool palette to be scrollable and searchable
+* Basic UI theming, add a real title and favicon lol
+
+## Additional functionality
+* Creating and using multi-emoji stamps from a stamp toolbox
+* Some means of persisting stamps
+* Some means of persisting, returning to, sharing grid designs
+* Import from slack copy-paste to grid
+* Dynamically get emoji from slack (per workspace? somehow?)
+
+## QoL
+* Toggle for click-to-place vs drag-to-place
--- src/emoji.ts
+++ src/emoji.ts
@@ -1,30 +1,18 @@
 
+const emojiSet: {[key: string]: string} = {
+  'hmmmm': 'https://emoji.slack-edge.com/T0D0DT1F0/hmmmm/7beb6f62ba141947.png',
+  'this': 'https://emoji.slack-edge.com/T0D0DT1F0/this/3c522ac8fd93caac.png',
+  'thonk_derp': 'https://emoji.slack-edge.com/T0D0DT1F0/thonk_derp/fcb552bff63169b3.png',
+  'yay-fox': 'https://emoji.slack-edge.com/T0D0DT1F0/yay-fox/bed4095a742619ad.gif',
+  'fried_lol': 'https://emoji.slack-edge.com/T0D0DT1F0/fried_lol/44b99ca264303644.jpg',
+  'stonks': 'https://emoji.slack-edge.com/T0D0DT1F0/stonks/282299594d655ec1.png',
+  'msn_h': 'https://emoji.slack-edge.com/T0D0DT1F0/msn_h/d76716b0f9008e20.gif',
+};
+
 export function getEmojiList() {
-  return [
-    'hmmmm',
-    'this',
-    'thonk_derp',
-    'yay-fox',
-    'fried_lol',
-    'stonks',
-  ];
+  return Object.keys(emojiSet);
 }
 
 export function getImageSourceForEmoji(emoji: string): string | null {
-  switch (emoji) {
-  case 'hmmmm':
-    return 'https://emoji.slack-edge.com/T0D0DT1F0/hmmmm/7beb6f62ba141947.png';
-  case 'this':
-    return 'https://emoji.slack-edge.com/T0D0DT1F0/this/3c522ac8fd93caac.png';
-  case 'thonk_derp':
-    return 'https://emoji.slack-edge.com/T0D0DT1F0/thonk_derp/fcb552bff63169b3.png';
-  case 'yay-fox':
-    return 'https://emoji.slack-edge.com/T0D0DT1F0/yay-fox/bed4095a742619ad.gif';
-  case 'fried_lol':
-    return 'https://emoji.slack-edge.com/T0D0DT1F0/fried_lol/44b99ca264303644.jpg';
-  case 'stonks':
-    return 'https://emoji.slack-edge.com/T0D0DT1F0/stonks/282299594d655ec1.png';
-  default:
-    return null;
-  }
+  return emojiSet[emoji] ?? null;
 }
