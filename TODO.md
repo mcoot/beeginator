b7a0cb518e682108113ded8617bd0a073a6bd979
Joseph Spearritt <joe.spearritt@live.com>
2022-10-01 10:54:13 +1000

    Renderer takes smallest non-empty area

--- TODO.md
+++ TODO.md
@@ -6,7 +6,7 @@ All this is a random hack out of boredom while in covid iso so dunno that I'll g
 
 * Larger (resizeable? full-page?) work grid area
 * Actual layout
-* Renderer automatically takes the smallest area containing emoji
+* [DONE] Renderer automatically takes the smallest area containing emoji
 * Take emoji list from a file (probably ok for now - integrating with slack API directly probably too complicated auth-wise for this revision)
 * Allow configuring what the blank emoji is
 * Re-work tool palette to be scrollable and searchable
--- src/renderToSlackString.ts
+++ src/renderToSlackString.ts
@@ -1,8 +1,37 @@
+function findBounds(gridData: (string | null)[][]): {left: number; top: number; right: number; bottom: number} {
+  let left = -1;
+  let top = -1;
+  let right = -1;
+  let bottom = -1;
+  for (let y = 0; y < gridData.length; y++) {
+    for (let x = 0; x < gridData[y].length; x++) {
+      if (gridData[y][x]) {
+        if (top === -1 || y < top) {
+          top = y;
+        }
+        if (bottom === -1 || y > bottom) {
+          bottom = y;
+        }
+        if (left === -1 || x < left) {
+          left = x;
+        }
+        if (right === -1 || x > right) {
+          right = x;
+        }
+      }
+    }
+  }
+
+  return {left, top, right, bottom};
+}
+
 export function renderToSlackString(gridData: (string | null)[][], blankEmoji: string): string {
   let result = '';
 
-  for (const row of gridData) {
-    result = result.concat(...row.map((emoji) => emoji ? `:${emoji}:` : `:${blankEmoji}:`), '\n');
+  const {left, top, right, bottom} = findBounds(gridData);
+
+  for (const row of gridData.slice(top, bottom + 1)) {
+    result = result.concat(...row.slice(left, right + 1).map((emoji) => emoji ? `:${emoji}:` : `:${blankEmoji}:`), '\n');
   }
 
   return result;
